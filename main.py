55c4c199d878edfc99f72c6a6a370ec158855ea5
LanceLabarrete <97266542+LanceLabarrete@users.noreply.github.com>
2024-03-22 23:07:21 +0800

    Updated normalize() function and able to print in binary and hex
    Features:
    1. Can support more or less than 7 digits
    2. Can move decimal dot to the left or right
    3. Can solve sign bit, combi, exponent cont, and coefficient cont
    
    Errors/missing features:
    1. Error: Cannot update base number if decimal moves to the right
    2. No GUI
    3. No print in text
    4. Error: While there is an option to choose round off method, the program is unable to ask the user.

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
+import math
 
+
+def main():
+    # Main function
+    stop = 0
+    while stop != 1:
+        user_input = float(input("Input a decimal number: "))
+        base_number = int(input("Input the base number: "))
+        user_input = list(str(user_input))
+        signBit = sign_bit(user_input[0])  # Gets the sign bit
+
+
+        if user_input[0] == '-':
+            user_input.remove('-')
+
+        user_input = normalize(user_input, base_number)
+        if user_input[-1] == "True":
+            user_input.pop()
+            base_number = int(user_input[-1])
+            user_input.pop()
+
+        elif user_input[-1] == "False":
+            user_input.pop()
+
+        output = [int(num) for num in user_input]
+        # -------------------------------------------
+
+        msb = most_significant_bit(output[0])
+        exponentPrime = exponent_prime_hexadecimal(base_number)
+        combination_field = find_combination_field(output[0], msb, exponentPrime)
+
+        exponent_continuation = get_exponent_continuation(exponentPrime)
+
+        coefficient_continuation = get_coefficient_field(output[1:])
+
+        output_binary(signBit, combination_field, exponent_continuation, coefficient_continuation)
+        output_hex(signBit, combination_field, exponent_continuation, coefficient_continuation)
+
+        # print("Sign bit: ", signBit)
+        # print("Combination field: ", combination_field)
+        # print("Exponent Continuation: ", exponent_continuation)
+        # print("Coefficient Continuation: ", coefficient_continuation)
+        # Use the above prints to test
+        stop = int(input("Do you want to quit?: "))  # Input 1 to end the loop. This is for test purposes
 def sign_bit(index):
     if index == '-':
         return 1
@@ -79,7 +123,7 @@ def get_coefficient_field(nums):
         updated_section1.append(section1[6])  # g
         updated_section1.append(section1[7])  # h
         updated_section1.append(0)  # v
-        updated_section1.append(section1[9])   # j
+        updated_section1.append(section1[9])  # j
         updated_section1.append(section1[10])  # k
         updated_section1.append(section1[11])  # m
     elif section1[0] == 0 and section1[4] == 0 and section1[8] == 1:
@@ -174,7 +218,7 @@ def get_coefficient_field(nums):
         updated_section2.append(section2[6])  # g
         updated_section2.append(section2[7])  # h
         updated_section2.append(0)  # v
-        updated_section2.append(section2[9])   # j
+        updated_section2.append(section2[9])  # j
         updated_section2.append(section2[10])  # k
         updated_section2.append(section2[11])  # m
 
@@ -280,22 +324,49 @@ def merge_num_list(nums):
 
 
 def normalize(decimalnumbers, basenumber):
+    isBaseNumberUpdated = "False"
     if decimalnumbers[-2] == '.' and decimalnumbers[-1] == '0':
         decimalnumbers.pop()
         decimalnumbers.pop()
-    else:
+    else: # move decimal dot to the right
+        temp = decimalnumbers
         decimalnumbers = move_decimal_dot(decimalnumbers)
+        basenumber = update_base_number(temp, basenumber)
+        isBaseNumberUpdated = "True"
         decimalnumbers.pop()
 
     count_digits = len(decimalnumbers)
-
-    if count_digits < 7:
+    if count_digits < 7:  # if there are less than 7 digits, pad zeros on the left
         max = 7
         while count_digits < max:
             decimalnumbers.insert(0, '0')
             count_digits += 1
 
-    return decimalnumbers
+    elif count_digits > 7:  # if there are less than 7 digits, move decimal dot to the right and round off
+        decimalnumbers.append('.')
+        while True:
+            if count_digits > 7:
+                indexM = decimalnumbers.index('.') - 1
+                indexN = decimalnumbers.index('.')
+                decimalnumbers[indexM], decimalnumbers[indexN] = decimalnumbers[indexN], decimalnumbers[indexM]
+                count_digits -= 1
+                basenumber += 1
+            else:
+                isBaseNumberUpdated = "True"
+                break
+
+        float_digits = list_to_float(decimalnumbers)
+        float_digits = round(float_digits)
+        # float_digits = which_rounding_method(float_digits) #lets the user decide which rounding method
+        decimalnumbers = list(str(float_digits))
+
+    if isBaseNumberUpdated == "True":
+        decimalnumbers.append(str(basenumber))
+        decimalnumbers.append(isBaseNumberUpdated)
+        return decimalnumbers
+    else:
+        decimalnumbers.append(isBaseNumberUpdated)
+        return decimalnumbers
 
 
 def move_decimal_dot(decimalnumbers):
@@ -309,31 +380,101 @@ def move_decimal_dot(decimalnumbers):
     return decimalnumbers
 
 
+def update_base_number(decimalnumbers, basenumber):
+    while True:
+        if decimalnumbers[-1] != '.':
+            indexM = decimalnumbers.index('.')
+            indexN = decimalnumbers.index('.') + 1
+            decimalnumbers[indexM], decimalnumbers[indexN] = decimalnumbers[indexN], decimalnumbers[indexM]
+            basenumber -= 1
+        else:
+            break
+    return basenumber
+
+
+def list_to_float(digits):
+    # Join the list elements into a single string
+    number_str = ''.join(digits)
+
+    # If there is no decimal point in the string, add one at the end
+    if '.' not in number_str:
+        number_str += '.'
+
+    # Convert the string to a float
+    return float(number_str)
+
+
+def output_binary(signbit, combinationfield, exponentcont, coefficientcont):
+    combinedstring1 = str(signbit)
+    combinedstring2 = ''.join(map(str, combinationfield))
+    combinedstring3 = ''.join(map(str, exponentcont))
+    combinedstring4 = ''.join(map(str, coefficientcont))
+
+    print(combinedstring1, combinedstring2, combinedstring3, combinedstring4)
+
+
+def output_hex(signbit, combinationfield, exponentcont, coefficientcont):
+    combined_list = [signbit]
+    combined_list.extend(combinationfield)
+    combined_list.extend(exponentcont)
+    combined_list.extend(coefficientcont)
+    hex_output = [hex_converter(combined_list[0:4]), hex_converter(combined_list[4:8]),
+                  hex_converter(combined_list[8:12]), hex_converter(combined_list[12:16]),
+                  hex_converter(combined_list[16:20]), hex_converter(combined_list[20:24]),
+                  hex_converter(combined_list[24:28]), hex_converter(combined_list[28:])]
+    final_output = ''.join(map(str, hex_output))
+    print(final_output)
+
+
+def hex_converter(decimaldigits):
+    if decimaldigits[0] == 0 and decimaldigits[1] == 0 and decimaldigits[2] == 0 and decimaldigits[3] == 0:
+        return '0'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 0 and decimaldigits[2] == 0 and decimaldigits[3] == 1:
+        return '1'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 0 and decimaldigits[2] == 1 and decimaldigits[3] == 0:
+        return '2'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 0 and decimaldigits[2] == 1 and decimaldigits[3] == 1:
+        return '3'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 1 and decimaldigits[2] == 0 and decimaldigits[3] == 0:
+        return '4'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 1 and decimaldigits[2] == 0 and decimaldigits[3] == 1:
+        return '5'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 1 and decimaldigits[2] == 1 and decimaldigits[3] == 0:
+        return '6'
+    elif decimaldigits[0] == 0 and decimaldigits[1] == 1 and decimaldigits[2] == 1 and decimaldigits[3] == 1:
+        return '7'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 0 and decimaldigits[2] == 0 and decimaldigits[3] == 0:
+        return '8'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 0 and decimaldigits[2] == 0 and decimaldigits[3] == 1:
+        return '9'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 0 and decimaldigits[2] == 1 and decimaldigits[3] == 0:
+        return 'A'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 0 and decimaldigits[2] == 1 and decimaldigits[3] == 0:
+        return 'B'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 0 and decimaldigits[2] == 1 and decimaldigits[3] == 1:
+        return 'C'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 1 and decimaldigits[2] == 0 and decimaldigits[3] == 1:
+        return 'D'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 1 and decimaldigits[2] == 1 and decimaldigits[3] == 0:
+        return 'E'
+    elif decimaldigits[0] == 1 and decimaldigits[1] == 1 and decimaldigits[2] == 1 and decimaldigits[3] == 1:
+        return 'F'
+
+
+def which_rounding_method(number):
+    if input == 0:  # nearest zero
+        if number >= 0:
+            return int(number)
+        else:
+            return -int(-number)
+    elif input == 1:  # floor
+        return math.floor(number)
+    elif input == 2:  # ceiling
+        return math.ceil(number)
+    elif input == 3:  # ties to nearest even
+        number = round(number)
+        return number
+
+
 if __name__ == '__main__':
-    # Main function
-    stop = 0
-    while stop != 1:
-        user_input = float(input("Input a decimal number: "))
-        base_number = int(input("Input the base number: "))
-        user_input = list(str(user_input))
-        signBit = sign_bit(user_input[0])  # Gets the sign bit
-        print("Sign bit: ", signBit)
-
-        if user_input[0] == '-':
-            user_input.remove('-')
-
-        user_input = normalize(user_input, base_number)
-        output = [int(num) for num in user_input]
-        # -------------------------------------------
-
-        msb = most_significant_bit(output[0])
-        exponentPrime = exponent_prime_hexadecimal(base_number)
-        combination_field = find_combination_field(output[0], msb, exponentPrime)
-        print("Combination field: ", combination_field)
-
-        exponent_continuation = get_exponent_continuation(exponentPrime)
-        print("Exponent Continuation: ", exponent_continuation)
-
-        coefficient_continuation = get_coefficient_field(output[1:])
-        print("Coefficient Continuation: ", coefficient_continuation)
-        stop = int(input("Do you want to quit?: "))  # Input 1 to end the loop. This is for test purposes
+    main()
